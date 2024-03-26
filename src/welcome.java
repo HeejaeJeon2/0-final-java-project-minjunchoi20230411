6cbc107dd8f33fd213a222601334fdb548354982
heejae <13478qw@naver.com>
2024-03-26 15:28:58 +0900

    5장끝

--- src/welcome.java
+++ src/welcome.java
@@ -19,12 +19,17 @@ public class welcome {
 			System.out.println("********************************");
 			System.out.println("\t" + greeting);
 			System.out.println("\t" + tagline);
+			
+			/* 
 			System.out.println("********************************");
 			System.out.println("1. 고객 정보 확인하기 \t5.바구니에 항목 추가하기");
 			System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니의 항목 수량 줄이기");
 			System.out.println("3. 장바구니 비우기 \t7. 장바구니의 항목 삭제하기");
 			System.out.println("4. 영수증 표시하기 \t8. 종료");
 			System.out.println("********************************");
+			*/
+			
+			menuIntro();
 			
 			System.out.println("메뉴 번호를 선택하세요: ");
 			int choice = input.nextInt();
@@ -36,29 +41,30 @@ public class welcome {
 			else {
 				switch(choice) {
 					case 1:
-						System.out.println("현재 고객 정보 : ");
-						System.out.println("이름" + name + " 연락처 " + phone);
+//						System.out.println("현재 고객 정보 : ");
+//						System.out.println("이름" + name + " 연락처 " + phone);
+						menuGuestInfo(name, phone);
 						break;
 					case 2:
-						System.out.println("장바구니 상품 목록 보기 : ");
+						menuCartItemList();
 						break;
 					case 3:
-						System.out.println("장바구니 비우기 : ");
+						menuCartClear();
 						break;
 					case 4:
-						System.out.println("장바구니 항목 추가하기 : ");
+						menuCartAddItem();
 						break;
 					case 5:
-						System.out.println("장바구니 항목 수량 줄이기 : ");
+						menuCartRemoveItemCount();
 						break;
 					case 6:
-						System.out.println("장바구니의 항목 삭제하기 : ");
+						menuCartRemoveItem();
 						break;
 					case 7:
-						System.out.println("영수증 표시하기 : ");
+						menuCartBill();
 						break;
 					case 8:
-						System.out.println("종료 : ");
+						menuCartExit();
 						quit = true;
 						break;
 					
@@ -66,6 +72,62 @@ public class welcome {
 			}
 		}
 	}
-
+	/**
+	 * 설명: Print Menu
+	 * 매개변수: 
+	 * 반환값: 
+	 * 
+	 */
+	public static void menuIntro() {
+		System.out.println("********************************");
+		System.out.println("1. 고객 정보 확인하기 \t5.바구니에 항목 추가하기");
+		System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니의 항목 수량 줄이기");
+		System.out.println("3. 장바구니 비우기 \t7. 장바구니의 항목 삭제하기");
+		System.out.println("4. 영수증 표시하기 \t8. 종료");
+		System.out.println("********************************");
+	}
+	
+	/**
+	 * 설명: 고객님 정보 출력
+	 * 매개변수: 
+	 *  - Strig	name	고객님의 이름
+	 *  - int	phone	휴대전화 번호
+	 * 반환값: 
+	 * 
+	 */
+	public static void menuGuestInfo(String name, int phone) {
+		System.out.println("현재 고객 정보 : ");
+		System.out.println("이름" + name + " 연락처 " + phone);
+	}
+	
+	/**
+	 * 설명: 2번
+	 * 매개변수: 
+	 *  - Strig	name	고객님의 이름
+	 *  - int	phone	휴대전화 번호
+	 * 반환값: 
+	 * 
+	 */
+	public static void menuCartItemList() {
+		System.out.println("2. 장바구니 상품 목록 보기 : ");
+	}
+	public static void menuCartClear() {
+		System.out.println("3. 장바구니 비우기 : ");
+	}
+	public static void menuCartAddItem() {
+		System.out.println("4. 장바구니 항목 추가하기 : ");
+	}
+	public static void menuCartRemoveItemCount() {
+		System.out.println("5. 장바구니 항목 수량 줄이기 : ");
+	}
+	public static void menuCartRemoveItem() {
+		System.out.println("6. 장바구니의 항목 삭제하기 : ");
+	}
+	public static void menuCartBill() {
+		System.out.println("7. 영수증 표시하기 : ");
+	}
+	public static void menuCartExit() {
+		System.out.println("8. 종료 : ");
+	}
 }
 
