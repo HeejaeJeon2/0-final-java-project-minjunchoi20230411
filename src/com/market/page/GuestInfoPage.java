86d0a05db2879bf1cda251f1fce912731c93664f
heejae <13478qw@naver.com>
2024-05-28 17:27:08 +0900

    ing

---
+++ src/com/market/page/GuestInfoPage.java
@@ -0,0 +1,13 @@
+package com.market.page;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GuestInfoPage extends JPanel {
+
+	public GuestInfoPage(JPanel panel) {
+		Font ft;
+		ft = new Font("함초롬돋움", Font.BOLD, 15);
+	}
+	
+}
